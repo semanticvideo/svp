ba9617abbcbfb94ac75107aed04ddeb82cf62df9
Dom Esposito <macmixing@gmail.com>
2026-07-28 12:24:31 -0700

    feat(models): define runtime model identity contract

---
+++ packages/svp-models/include/svp/models/reference_processor_model_ids.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace svp::models {
+
+inline constexpr char kWhisperSmallEnglishModelId[] = "model_whisper_small_en";
+inline constexpr char kSherpaOnnxDiarizationModelId[] =
+    "model_sherpa_onnx_diarization";
+inline constexpr char kDepthAnythingV2SmallModelId[] =
+    "model_depth_anything_v2_small";
+inline constexpr char kNomicEmbedTextV15ModelId[] =
+    "model_nomic_embed_text_v1_5";
+inline constexpr char kNomicEmbedVisionV15ModelId[] =
+    "model_nomic_embed_vision_v1_5";
+inline constexpr char kRfDetrNanoCocoModelId[] = "model_rfdetr_nano_coco";
+inline constexpr char kPpOcrV6MediumDetectorModelId[] =
+    "model_pp_ocrv6_medium_det";
+inline constexpr char kPpOcrV6MediumRecognizerModelId[] =
+    "model_pp_ocrv6_medium_rec";
+
+}  // namespace svp::models
